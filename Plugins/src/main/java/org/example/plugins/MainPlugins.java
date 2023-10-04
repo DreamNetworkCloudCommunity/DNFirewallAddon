0c20795cf788803d4964a854326834a5df87b07b
Alexandre01 <34719450+Alexandre01Dev@users.noreply.github.com>
2023-10-04 17:28:32 +0200

    Initial commit

---
+++ Plugins/src/main/java/org/example/plugins/MainPlugins.java
@@ -0,0 +1,11 @@
+package org.example.plugins;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MainPlugins extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("Hello World!");
+    }
+}
